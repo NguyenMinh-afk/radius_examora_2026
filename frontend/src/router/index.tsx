2f1a2afce807e9b38d39d86ff92541506fb10eae
NoctZenn <minhnd.22810310235@epu.edu.vn>
2026-04-01 16:00:20 +0700

    Update UI Dashboard

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -5,6 +5,8 @@ import Contact from "../pages/Contact/Contact";
 import Register from "../pages/Register/Register";
 import Login from "../pages/Login/Login";
 import Forgot from "../pages/Forgot/Forgot";
+import AdminDashboard from "../pages/Dashboard/Admin/AdminDashboard";
+import TeacherDashboard from "../pages/Dashboard/Teacher/TeacherDashboard";
 
 const AppRouter = () => {
   return (
@@ -25,6 +27,12 @@ const AppRouter = () => {
         
         {/* Forgot Password */}
         <Route path="/forgot-password" element={<Forgot />} />
+
+        {/* Admin Dashboard */}
+        <Route path="/admin" element={<AdminDashboard />} />
+
+        {/* Teacher Dashboard */}
+        <Route path="/teacher" element={<TeacherDashboard />} /> 
 
       </Routes>
     </BrowserRouter>
